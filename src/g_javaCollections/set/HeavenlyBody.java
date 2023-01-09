9fd28528bfc0a477ead56d08d1fb60734cb47d63
Barbara Burakowska <bnburakowska@gmail.com>
2023-01-09 13:21:52 +0100

    Set/HashSet

---
+++ src/g_javaCollections/set/HeavenlyBody.java
@@ -0,0 +1,32 @@
+package g_javaCollections.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HeavenlyBody {
+    private final String name;
+    private final double orbitalPeriod;
+    private final Set<HeavenlyBody> satellites;
+
+    public HeavenlyBody(String name, double orbinalPeriod) {
+        this.name = name;
+        this.orbitalPeriod = orbinalPeriod;
+        this.satellites = new HashSet<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getOrbitalPeriod() {
+        return orbitalPeriod;
+    }
+
+    public boolean addMoon(HeavenlyBody moon) {
+        return this.satellites.add(moon);
+    }
+
+    public Set<HeavenlyBody> getSatellites() {
+        return new HashSet<>(this.satellites);
+    }
+}
